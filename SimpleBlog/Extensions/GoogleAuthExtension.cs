7eb2e430d17867930167cfcf5840562c5eed2f30
hosein <iraji.work@gmail.com>
2025-05-31 21:02:20 +0330

    Add project files.

---
+++ SimpleBlog/Extensions/GoogleAuthExtension.cs
@@ -0,0 +1,41 @@
+﻿using Google.Apis.Auth.AspNetCore3;
+using Microsoft.AspNetCore.Authentication.Cookies;
+using System.Dynamic;
+
+namespace SimpleBlog.Settings
+{
+    public static class GoogleAuthExtension
+    {
+        /// <summary>
+        /// Reads config from configuration and setup authentication using google auth
+        /// </summary>
+        /// <param name="services"></param>
+        /// <param name="configuration"></param>
+        /// <returns></returns>
+        /// <exception cref="NullReferenceException"></exception>
+        public static IServiceCollection AddGoogleAuthentication(this IServiceCollection services, IConfiguration configuration)
+        {
+            string? clientId = configuration["Authentication:Google:ClientId"];
+            string? secret = configuration["Authentication:Google:Secret"];
+            if(string.IsNullOrEmpty(clientId) || string.IsNullOrEmpty(secret))
+                throw new ArgumentNullException("Invalid Google auth configuration");
+
+            services.AddAuthentication(auth =>
+            {
+                auth.DefaultChallengeScheme = GoogleOpenIdConnectDefaults.AuthenticationScheme;
+                auth.DefaultForbidScheme = GoogleOpenIdConnectDefaults.AuthenticationScheme;
+                auth.DefaultScheme = CookieAuthenticationDefaults.AuthenticationScheme;
+            })
+                    .AddCookie()
+                    .AddGoogleOpenIdConnect(options =>
+                    {
+                        options.ClientId = clientId;
+                        options.ClientSecret = secret;
+                    })
+
+                ;
+
+            return services;
+        }
+    }
+}
