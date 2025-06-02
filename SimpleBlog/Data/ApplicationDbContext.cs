feb02ce43986d298a05307652c979eb9857da682
hosein <iraji.work@gmail.com>
2025-06-02 11:16:36 +0330

    seed blogPost data

--- SimpleBlog/Data/ApplicationDbContext.cs
+++ SimpleBlog/Data/ApplicationDbContext.cs
@@ -29,6 +29,9 @@ namespace SimpleBlog.Data
                 new Content() { Id = 1, Token = "technology" , EntityType = Framework.ContentEntityType.BLogCategory},
                 new Content() { Id = 2, Token = "music",  EntityType = Framework.ContentEntityType.BLogCategory},
                 new Content() { Id = 3, Token = "Financial tips", EntityType = Framework.ContentEntityType.BLogCategory},
+                new Content() { Id = 4, Token = "First story", EntityType = Framework.ContentEntityType.BlogPost},
+                new Content() { Id = 5, Token = "Second story", EntityType = Framework.ContentEntityType.BlogPost},
+                new Content() { Id = 6, Token = "Third story", EntityType = Framework.ContentEntityType.BlogPost},
                 ]);
 
             var contentBuilder = new ContentBuilder();
@@ -40,6 +43,19 @@ namespace SimpleBlog.Data
                 contentBuilder.NewBlogCategoryBuilder().SetTitle("Financial tips").SetId(3).SetContentId(3).Build(),
             };
             builder.Entity<BlogCagtegory>().HasData(seedCategoryList);
+
+            var initUser = new User() { Id = Guid.NewGuid().ToString(), UserName = "admin", Email = "admin@website.com" };
+
+            builder.Entity<User>().HasData(initUser);
+
+            builder.Entity<BlogPost>().HasData([
+                contentBuilder.NewBlogPostBuilder().SetId(1).SetCategoryId(1).SetAuthorId(initUser.Id).SetContentId(4).SetTitle("First story").SetBody("Sample text 1").Build(),
+                contentBuilder.NewBlogPostBuilder().SetId(2).SetCategoryId(1).SetAuthorId(initUser.Id).SetContentId(4).SetTitle("First story - edited").SetBody("Sample text 1 - edited").SetActiveVersion(true).Build(),
+                contentBuilder.NewBlogPostBuilder().SetId(3).SetCategoryId(1).SetAuthorId(initUser.Id).SetContentId(5).SetTitle("Second story").SetBody("Sample text 2").Build(),
+                contentBuilder.NewBlogPostBuilder().SetId(4).SetCategoryId(1).SetAuthorId(initUser.Id).SetContentId(5).SetTitle("Second story - edited").SetBody("Sample text 2 - edited").SetActiveVersion(true).Build(),
+                contentBuilder.NewBlogPostBuilder().SetId(5).SetCategoryId(1).SetAuthorId(initUser.Id).SetContentId(6).SetTitle("Third story").SetBody("Sample text 3").Build(),
+                contentBuilder.NewBlogPostBuilder().SetId(6).SetCategoryId(1).SetAuthorId(initUser.Id).SetContentId(6).SetTitle("Third story - edited").SetBody("Sample text 3 - edited").SetActiveVersion(true).Build(),
+                ]);
 
 
             //builder.Entity<BlogCagtegory>().HasData([
